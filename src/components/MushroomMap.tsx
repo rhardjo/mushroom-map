7eac1de7da6449d216b030f08276278bbc3c2784
Rano Hardjosemito <r.hardjo@live.nl>
2020-03-03 14:18:21 +0100

    Use styled-components

--- src/components/MushroomMap.tsx
+++ src/components/MushroomMap.tsx
@@ -1,18 +1,22 @@
-import './MushroomMap.css';
-
 import React from 'react';
+import styled from 'styled-components';
 import { Map, TileLayer } from 'react-leaflet';
 
 import MushroomMarkerList from './MushroomMarkerList';
 
+const StyledMap = styled(Map)`
+  width: 100%;
+  height: 100vh;
+`;
+
 const MushroomMap = ({ mushrooms }: any) => (
-  <Map className="MushroomMap" center={[52.080861, 5.235876]} zoom={18}>
+  <StyledMap className="MushroomMap" center={[52.080861, 5.235876]} zoom={18}>
     <TileLayer
       attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
     <MushroomMarkerList mushrooms={mushrooms} />
-  </Map>
+  </StyledMap>
 );
 
 export default MushroomMap;
--- src/components/MushroomMarker.tsx
+++ src/components/MushroomMarker.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Marker, Popup } from 'react-leaflet';
 
-import { Mushroom } from '../api';
+import { Mushroom, Spots, Color } from '../api';
 
-const MushroomMarker = ({ latlng, ...mushroomProps }: Mushroom) => {
-  console.log(mushroomProps);
+interface IMushroomPopup {
+  borderStyle: string;
+  borderColor: string;
+}
+
+const MushroomName = styled.h2`
+  text-transform: capitalize;
+`;
+const MushroomStyle = styled.div`
+  margin: 0;
+`;
+const MushroomColor = styled.div`
+  text-transform: capitalize;
+`;
+const MushroomPopup = styled(Popup)<IMushroomPopup>`
+  .leaflet-popup-content-wrapper {
+    border: 3px ${(props) => props.borderStyle} ${(props) => props.borderColor};
+  }
+`;
+
+const MushroomMarker = ({ latlng, name, spots, color }: Mushroom) => {
+  const mushroomColor = Color[color];
+  const mushroomStyle = Spots[spots];
   return (
     <Marker position={latlng}>
-      <Popup>{mushroomProps.name}</Popup>
+      <MushroomPopup borderColor={mushroomColor} borderStyle={mushroomStyle}>
+        <MushroomName>{name}</MushroomName>
+        <MushroomStyle>{`Style: ${mushroomStyle}`}</MushroomStyle>
+        <MushroomColor>{`Color: ${mushroomColor}`}</MushroomColor>
+      </MushroomPopup>
     </Marker>
   );
 };
